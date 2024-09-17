682ab79aceb3328f27675370b8d6e4b70cbb2637
samarjeet-singh-kheda <samarjeetsingh259@gmail.com>
2024-09-17 20:27:04 +0530

    FEAT: undo buttons & triple like button;  LEARNT: State update batching, events

--- src/components/TabContent.js
+++ src/components/TabContent.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { flushSync } from "react-dom";
 
 export default function TabContent({ item }) {
   console.log("TAB CONTENT component rendered");
@@ -7,7 +8,57 @@ export default function TabContent({ item }) {
   const [likes, setLikes] = useState(0);
 
   function handleInc() {
-    setLikes(likes + 1);
+    setLikes((likes) => {
+      console.log(likes);
+      return likes + 1;
+    });
+  }
+
+  function handleTripleInc() {
+    setLikes((likes) => {
+      console.log(likes);
+      return likes + 1;
+    });
+
+    setLikes((likes) => {
+      console.log(likes);
+      return likes + 1;
+    });
+
+    setLikes((likes) => {
+      console.log(likes);
+      return likes + 1;
+    });
+  }
+
+  function handleUndo() {
+    console.log("BEFORE Undo", likes);
+
+    flushSync(() => {
+      console.log("BEFORE setLikes:", likes);
+      setLikes(0);
+      console.log("AFTER setLikes:", likes);
+    });
+
+    // flushSync(() => {
+    //   // console.log("BEFORE setShowDetails:", likes);
+    //   // setShowDetails(true);
+    //   // console.log("AFTER setShowDetails:", likes);
+
+    //   setShowDetails(() => {
+    //     console.log("In setShowDetails:", likes);
+    //     return true;
+    //   });
+    // });
+
+    setLikes(0);
+    setShowDetails(true);
+
+    console.log("AFTER Undo", likes);
+  }
+
+  function handleUndoLater() {
+    setTimeout(handleUndo, 2000);
   }
 
   return (
@@ -23,13 +74,13 @@ export default function TabContent({ item }) {
         <div className="hearts-counter">
           <span>{likes} ❤️</span>
           <button onClick={handleInc}>+</button>
-          <button>+++</button>
+          <button onClick={handleTripleInc}>+++</button>
         </div>
       </div>
 
       <div className="tab-undo">
-        <button>Undo</button>
-        <button>Undo in 2s</button>
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleUndoLater}>Undo in 2s</button>
       </div>
     </div>
   );
